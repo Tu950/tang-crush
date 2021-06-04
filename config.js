1ef5807f3695cc5796d8d707bb26853b30a91555
Tu950 <85305186+Tu950@users.noreply.github.com>
2021-06-04 12:45:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,9 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Mh ạ!',
+    introDesc: `Anh thấy em có vẻ không hợp làm nóc nhà đâu, để anh lên làm thay cho 😉`,
+    btnIntro: '.',
+    title: 'Đây click để trả lời nè ',
+    desc: 'Đồng ý đi huhu ',
+    btnYes: 'Đồng ý',
+    btnNo: 'Không đồng ý',
 }
